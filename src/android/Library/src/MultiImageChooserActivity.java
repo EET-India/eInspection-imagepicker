7e1e28a4ffc610efc67e100f800c1055a3578398
divyasakpal <divyasakpal14@gmail.com>
2016-07-05 14:27:00 +0530

    Update MultiImageChooserActivity.java

--- src/android/Library/src/MultiImageChooserActivity.java
+++ src/android/Library/src/MultiImageChooserActivity.java
@@ -124,7 +124,7 @@ public class MultiImageChooserActivity extends Activity implements OnItemClickLi
         fakeR = new FakeR(this);
         setContentView(fakeR.getId("layout", "multiselectorgrid"));
         fileNames.clear();
-       // setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR); 
+       setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR); 
      //  MultiImageChooserActivity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
        //  this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         maxImages = getIntent().getIntExtra(MAX_IMAGES_KEY, NOLIMIT);
