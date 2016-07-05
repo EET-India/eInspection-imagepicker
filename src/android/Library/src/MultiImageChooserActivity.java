bac22d402fe488d4153fe6c7d3a998bf5e1b1dac
divyasakpal <divyasakpal14@gmail.com>
2016-07-05 15:14:43 +0530

    Update MultiImageChooserActivity.java

--- src/android/Library/src/MultiImageChooserActivity.java
+++ src/android/Library/src/MultiImageChooserActivity.java
@@ -126,8 +126,8 @@ public class MultiImageChooserActivity extends Activity implements OnItemClickLi
         fileNames.clear();
        // ActivityInfo Ac= info.activityInfo;
       // setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR); 
-      //(MultiImageChooserActivity.this).setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+      (MultiImageChooserActivity.this).setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         maxImages = getIntent().getIntExtra(MAX_IMAGES_KEY, NOLIMIT);
         desiredWidth = getIntent().getIntExtra(WIDTH_KEY, 0);
         desiredHeight = getIntent().getIntExtra(HEIGHT_KEY, 0);
