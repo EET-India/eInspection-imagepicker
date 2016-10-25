3c536d03653bc704aaa8e981a02bd4d62b2b4bd0
aishbhor <aishbhor@users.noreply.github.com>
2016-10-25 18:05:04 +0530

    Update ImagePicker.java

--- src/android/com/synconset/ImagePicker/ImagePicker.java
+++ src/android/com/synconset/ImagePicker/ImagePicker.java
@@ -4,8 +4,10 @@
 package com.synconset;
 
 import org.apache.cordova.PermissionHelper;
+
 import android.Manifest;
 import android.content.pm.PermissionInfo;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 
@@ -20,66 +22,66 @@ import android.content.Intent;
 import android.util.Log;
 
 public class ImagePicker extends CordovaPlugin {
-	public static String TAG = "ImagePicker";
-	 
-	private CallbackContext callbackContext;
-	private JSONObject params;
-	 
-	public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
-		 this.callbackContext = callbackContext;
-		 this.params = args.getJSONObject(0);
-		
-		if (action.equals("getPictures")) {
-			try {	       
-				 PermissionHelper.requestPermission(this, 1, Manifest.permission.READ_EXTERNAL_STORAGE);
-			}
-			catch (IllegalArgumentException e)
-			{
-				callbackContext.error("Illegal Argument Exception");
-				return false;
-			}
-			
-			Intent intent = new Intent(cordova.getActivity(), MultiImageChooserActivity.class);
-			int max = 20;
-			int desiredWidth = 0;
-			int desiredHeight = 0;
-			int quality = 100;
-			if (this.params.has("maximumImagesCount")) {
-				max = this.params.getInt("maximumImagesCount");
-			}
-			if (this.params.has("width")) {
-				desiredWidth = this.params.getInt("width");
-			}
-			if (this.params.has("height")) {
-				desiredHeight = this.params.getInt("height");
-			}
-			if (this.params.has("quality")) {
-				quality = this.params.getInt("quality");
-			}
-			intent.putExtra("MAX_IMAGES", max);
-			intent.putExtra("WIDTH", desiredWidth);
-			intent.putExtra("HEIGHT", desiredHeight);
-			intent.putExtra("QUALITY", quality);
-			if (this.cordova != null) {
-				this.cordova.startActivityForResult((CordovaPlugin) this, intent, 0);
-			}
-		}
-		return true;
-	}
-	
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (resultCode == Activity.RESULT_OK && data != null) {
-			ArrayList<String> fileNames = data.getStringArrayListExtra("MULTIPLEFILENAMES");
-			JSONArray res = new JSONArray(fileNames);
-			this.callbackContext.success(res);
-		} else if (resultCode == Activity.RESULT_CANCELED && data != null) {
-			String error = data.getStringExtra("ERRORMESSAGE");
-			this.callbackContext.error(error);
-		} else if (resultCode == Activity.RESULT_CANCELED) {
-			JSONArray res = new JSONArray();
-			this.callbackContext.success(res);
-		} else {
-			this.callbackContext.error("No images selected");
-		}
-	}
+    public static String TAG = "ImagePicker";
+    protected final static String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE};
+    private CallbackContext callbackContext;
+    private JSONObject params;
+
+    public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        this.callbackContext = callbackContext;
+        this.params = args.getJSONObject(0);
+
+        if (action.equals("getPictures")) {
+            Intent intent = new Intent(cordova.getActivity(), MultiImageChooserActivity.class);
+            int max = 20;
+            int desiredWidth = 0;
+            int desiredHeight = 0;
+            int quality = 100;
+            if (this.params.has("maximumImagesCount")) {
+                max = this.params.getInt("maximumImagesCount");
+            }
+            if (this.params.has("width")) {
+                desiredWidth = this.params.getInt("width");
+            }
+            if (this.params.has("height")) {
+                desiredHeight = this.params.getInt("height");
+            }
+            if (this.params.has("quality")) {
+                quality = this.params.getInt("quality");
+            }
+            intent.putExtra("MAX_IMAGES", max);
+            intent.putExtra("WIDTH", desiredWidth);
+            intent.putExtra("HEIGHT", desiredHeight);
+            intent.putExtra("QUALITY", quality);
+            if (this.cordova != null) {
+                try {
+                    if (!PermissionHelper.hasPermission(this, permissions[0])) {
+                        PermissionHelper.requestPermission(this, 1, Manifest.permission.READ_EXTERNAL_STORAGE);
+                    } else {
+                        this.cordova.startActivityForResult((CordovaPlugin) this, intent, 0);
+                    }
+                } catch (IllegalArgumentException e) {
+                    callbackContext.error("Illegal Argument Exception");
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            ArrayList<String> fileNames = data.getStringArrayListExtra("MULTIPLEFILENAMES");
+            JSONArray res = new JSONArray(fileNames);
+            this.callbackContext.success(res);
+        } else if (resultCode == Activity.RESULT_CANCELED && data != null) {
+            String error = data.getStringExtra("ERRORMESSAGE");
+            this.callbackContext.error(error);
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            JSONArray res = new JSONArray();
+            this.callbackContext.success(res);
+        } else {
+            this.callbackContext.error("No images selected");
+        }
+    }
 }
